c540fae4f9a2eceb02f2fbd8cad9bb493ab945b5
Tsoji <tsojivictor@gmail.com>
2024-11-14 11:25:42 +0100

    authentication

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 // Firebase config
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.extra.apiKey,
-  authDomain: Constants.expoConfig.extra.authDomain,
-  projectId: Constants.expoConfig.extra.projectId,
-  storageBucket: Constants.expoConfig.extra.storageBucket,
-  messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
-  appId: Constants.expoConfig.extra.appId,
-  databaseURL: Constants.expoConfig.extra.databaseURL,
+  apiKey: "AIzaSyCp7QF3XdCtnxNxqZiuRfCb1Om3LqH2GPQ",
+  authDomain: "tutormama-4888b.firebaseapp.com",
+  databaseURL: "https://tutormama-4888b-default-rtdb.firebaseio.com",
+  projectId: "tutormama-4888b",
+  storageBucket: "tutormama-4888b.firebasestorage.app",
+  messagingSenderId: "756880604092",
+  appId: "1:756880604092:web:2f9b967fea27e5929b09b9",
+  measurementId: "G-78NYYX1B6E"
   //   @deprecated is deprecated Constants.manifest
 };
 // initialize firebase
